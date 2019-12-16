5763478c571dfddc7900290dfb51e79470a8ebeb
github username <github email address>
2019-12-16 15:01:23 -0500

    changes added

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,23 @@
+class TrainersController < ApplicationController
+
+    def index
+        trainers = Trainer.all 
+        # render json: trainers, include: [:pokemons], except: [:created_at, :updated_at]
+
+        render json: trainers.to_json(:include => {
+            :pokemons => {:only => [:id, :nickname, :species, :trainer_id ]} 
+        }, except: [:created_at, :updated_at] ) 
+
+      
+
+    end 
+
+
+      def show
+        trainer = Trainer.find_by(id: params[:id])
+        render json: {id: trainer.id, name: trainer.name} 
+      end
+
+
+end
+
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,13 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+})
+
+
+
+
