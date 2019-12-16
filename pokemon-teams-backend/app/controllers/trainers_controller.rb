66531912660e5934f84dead717c0801e334f999d
github username <github email address>
2019-12-16 15:49:19 -0500

    more changes

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -3,13 +3,9 @@ class TrainersController < ApplicationController
     def index
         trainers = Trainer.all 
         # render json: trainers, include: [:pokemons], except: [:created_at, :updated_at]
-
         render json: trainers.to_json(:include => {
             :pokemons => {:only => [:id, :nickname, :species, :trainer_id ]} 
         }, except: [:created_at, :updated_at] ) 
-
-      
-
     end 
 
 
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -5,9 +5,39 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 
 document.addEventListener("DOMContentLoaded", function() {
+    getAllPokemonTeams()
+} )
 
-})
+
+function getAllPokemonTeams(e) {
+    // e.preventDefault()
+     fetch(TRAINERS_URL)
+    .then(response => response.json())
+    .then(AllTrainersPokemon => AllTrainersPokemon.forEach(trainer => renderTrainer(trainer)) )
+    console.log(" in the get pokemons")
+}
 
 
+function renderTrainer(trainer) {
 
+    let mainTag = document.querySelector("main")
+    let pokemonCard = document.createElement("div")
+    pokemonCard.setAttribute("data-id",trainer.id)
+    pokemonCard.classList.add("card")
+
+    let trainerName = document.createElement("p") 
+    trainerName.innerText = trainer.name 
+    let trainerButton = document.createElement("button")
+    trainerButton.setAttribute("data-trainer-id" , trainer.id)
+    let trainerMainList = document.createElement("ul")
+    let individualTrainer = document.createElement("li")
+
+    mainTag.appendChild(pokemonCard)
+    pokemonCard.appendChild(trainerName)
+    pokemonCard.appendChild(trainerButton) 
+
+    console.log(trainer)
+
+
+}
 
